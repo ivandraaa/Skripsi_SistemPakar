ae2e43de8009875739b8faee0c4a3837df53cbcc
ivandraaa <ivandrafawwaz@gmail.com>
2024-01-06 19:20:29 +0700

    Clean Code

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="en-US" class="no-js no-svg">
-<!-- Mirrored from screening.mhanational.org/screening-tools/depression/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 01 Jan 2023 15:03:57 GMT -->
-<!-- Added by HTTrack -->
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 
 <head>
@@ -402,146 +400,8 @@
         media='all' />
     <script src='pasal-assets/wp-includes/js/jquery/jquery.mina7a0.js?ver=3.6.1' id='jquery-core-js'></script>
     <script src='pasal-assets/wp-includes/js/jquery/jquery-migrate.mind617.js?ver=3.3.2' id='jquery-migrate-js'></script>
-    <!--[if lt IE 9]> <script src='https://screening.mhanational.org/wp-content/themes/mha_s2s/assets/js/html5.js?ver=3.7.3' id='html5-js'>
-    </script> <![endif]-->
     <script defer='defer' src='pasal-assets/wp-content/plugins/gravityforms/js/jquery.json.min91ac.js?ver=2.6.8'
         id='gform_json-js'></script>
-    <script id='gform_gravityforms-js-extra'>
-        var gf_global = {
-            "gf_currency_config": {
-                "name": "U.S. Dollar",
-                "symbol_left": "$",
-                "symbol_right": "",
-                "symbol_padding": "",
-                "thousand_separator": ",",
-                "decimal_separator": ".",
-                "decimals": 2,
-                "code": "USD"
-            },
-            "base_url": "https:\/\/screening.mhanational.org\/wp-content\/plugins\/gravityforms",
-            "number_formats": [],
-            "spinnerUrl": "https:\/\/screening.mhanational.org\/wp-content\/plugins\/gravityforms\/images\/spinner.svg",
-            "strings": {
-                "newRowAdded": "New row added.",
-                "rowRemoved": "Row removed",
-                "formSaved": "The form has been saved.  The content contains the link to return and complete the form."
-            }
-        };
-        var gform_i18n = {
-            "datepicker": {
-                "days": {
-                    "monday": "Mon",
-                    "tuesday": "Tue",
-                    "wednesday": "Wed",
-                    "thursday": "Thu",
-                    "friday": "Fri",
-                    "saturday": "Sat",
-                    "sunday": "Sun"
-                },
-                "months": {
-                    "january": "January",
-                    "february": "February",
-                    "march": "March",
-                    "april": "April",
-                    "may": "May",
-                    "june": "June",
-                    "july": "July",
-                    "august": "August",
-                    "september": "September",
-                    "october": "October",
-                    "november": "November",
-                    "december": "December"
-                },
-                "firstDay": 1,
-                "iconText": "Select date"
-            }
-        };
-        var gf_legacy_multi = {
-            "1": "1"
-        };
-        var gform_gravityforms = {
-            "strings": {
-                "invalid_file_extension": "This type of file is not allowed. Must be one of the following:",
-                "delete_file": "Delete this file",
-                "in_progress": "in progress",
-                "file_exceeds_limit": "File exceeds size limit",
-                "illegal_extension": "This type of file is not allowed.",
-                "max_reached": "Maximum number of files reached",
-                "unknown_error": "There was a problem while saving the file on the server",
-                "currently_uploading": "Please wait for the uploading to complete",
-                "cancel": "Cancel",
-                "cancel_upload": "Cancel this upload",
-                "cancelled": "Cancelled"
-            },
-            "vars": {
-                "images_url": "https:\/\/screening.mhanational.org\/wp-content\/plugins\/gravityforms\/images"
-            }
-        };
-    </script>
-    <script id='gform_gravityforms-js-before'>
-        var gform;
-        gform || (document.addEventListener("gform_main_scripts_loaded", function() {
-            gform.scriptsLoaded = !0
-        }), window.addEventListener("DOMContentLoaded", function() {
-            gform.domLoaded = !0
-        }), gform = {
-            domLoaded: !1,
-            scriptsLoaded: !1,
-            initializeOnLoaded: function(o) {
-                gform.domLoaded && gform.scriptsLoaded ? o() : !gform.domLoaded && gform.scriptsLoaded ?
-                    window.addEventListener("DOMContentLoaded", o) : document.addEventListener(
-                        "gform_main_scripts_loaded", o)
-            },
-            hooks: {
-                action: {},
-                filter: {}
-            },
-            addAction: function(o, n, r, t) {
-                gform.addHook("action", o, n, r, t)
-            },
-            addFilter: function(o, n, r, t) {
-                gform.addHook("filter", o, n, r, t)
-            },
-            doAction: function(o) {
-                gform.doHook("action", o, arguments)
-            },
-            applyFilters: function(o) {
-                return gform.doHook("filter", o, arguments)
-            },
-            removeAction: function(o, n) {
-                gform.removeHook("action", o, n)
-            },
-            removeFilter: function(o, n, r) {
-                gform.removeHook("filter", o, n, r)
-            },
-            addHook: function(o, n, r, t, i) {
-                null == gform.hooks[o][n] && (gform.hooks[o][n] = []);
-                var e = gform.hooks[o][n];
-                null == i && (i = n + "_" + e.length), gform.hooks[o][n].push({
-                    tag: i,
-                    callable: r,
-                    priority: t = null == t ? 10 : t
-                })
-            },
-            doHook: function(n, o, r) {
-                var t;
-                if (r = Array.prototype.slice.call(r, 1), null != gform.hooks[n][o] && ((o = gform.hooks[n][
-                        o
-                    ]).sort(function(o, n) {
-                        return o.priority - n.priority
-                    }), o.forEach(function(o) {
-                        "function" != typeof(t = o.callable) && (t = window[t]), "action" == n ? t
-                            .apply(null, r) : r[0] = t.apply(null, r)
-                    })), "filter" == n) return r[0]
-            },
-            removeHook: function(o, n, t, i) {
-                var r;
-                null != gform.hooks[o][n] && (r = (r = gform.hooks[o][n]).filter(function(o, n, r) {
-                    return !!(null != i && i != o.tag || null != t && t != o.priority)
-                }), gform.hooks[o][n] = r)
-            }
-        });
-    </script>
     <script defer='defer' src='pasal-assets/wp-content/plugins/gravityforms/js/gravityforms.min91ac.js?ver=2.6.8'
         id='gform_gravityforms-js'></script>
     <script id='gform_conditional_logic-js-extra'>
@@ -551,9 +411,6 @@
     </script>
     <script defer='defer' src='pasal-assets/wp-content/plugins/gravityforms/js/conditional_logic.min91ac.js?ver=2.6.8'
         id='gform_conditional_logic-js'></script>
-    <link rel="https://api.w.org/" href="https://screening.mhanational.org/wp-json/" />
-    <link rel="alternate" type="application/json" href="https://screening.mhanational.org/wp-json/wp/v2/screen/22" />
-    <link rel='shortlink' href='https://screening.mhanational.org/?p=22' />
     <script>
         (function($) {
 
@@ -648,13 +505,13 @@
 <body class="screen-template-default single single-screen postid-22 not-front"> <noscript><iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-N3TWZFC" height="0" width="0"
             style="display:none;visibility:hidden"></iframe></noscript>
-            <!-- ======= Top Bar ======= -->
+    <!-- ======= Top Bar ======= -->
     <div id="page" class="site">
         <header id="header" class="clearfix">
             <div class="wrap normal"> <a id="logo" href="/"><img
-                        src="pasal-assets/wp-content/themes/mha_s2s/assets/images/mha-logo.png"
-                        alt="MHA Screening" /></a>
-                
+                        src="pasal-assets/wp-content/themes/mha_s2s/assets/images/logo.png"
+                        alt="Logo Sistem Pakar" /></a>
+
                 <nav id="navigation" class="main-navigation" role="navigation" aria-label="Top Menu">
                     <div class="menu-main-menu-container">
                         <ul id="main-menu" class="sf-menu">
@@ -680,10 +537,11 @@
                             <li>
                                 <i>
                                     @auth
-                                        <p class="mt-3" id="welcome-message" style="cursor: pointer; color: #009244; font-size: 14px;">Selamat datang,
+                                        <p class="mt-3" id="welcome-message"
+                                            style="cursor: pointer; color: #009244; font-size: 14px;">Selamat datang,
                                             {{ auth()->user()->name }}! (Klik untuk logout)</p>
                                     @endauth
-                                </i>    
+                                </i>
                             </li>
                         </ul>
                     </div>
