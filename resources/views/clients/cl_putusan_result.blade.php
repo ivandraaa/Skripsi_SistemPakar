d537dfdca00c31cb07620194806fdaecdd94fa39
ivandraaa <ivandrafawwaz@gmail.com>
2024-02-04 19:33:26 +0700

    Add All Result Identification

--- resources/views/clients/cl_putusan_result.blade.php
+++ resources/views/clients/cl_putusan_result.blade.php
@@ -26,7 +26,8 @@
         <div class="contact-info d-flex align-items-center">
             <i>
                 @auth
-                    <p class="mt-3" id="welcome-message" style="cursor: pointer; color: #009244; font-size: 14px;">Selamat datang,
+                    <p class="mt-3" id="welcome-message" style="cursor: pointer; color: #009244; font-size: 14px;">Selamat
+                        datang,
                         {{ auth()->user()->name }}! (Klik untuk logout)</p>
                 @endauth
             </i>
@@ -64,92 +65,148 @@
             </div>
 
             {{-- section 2 --}}
-            <div class="row">
-                <div class="">
-                    <div class="d-flex"> 
-                        {{-- Pakar --}}
-                        <table class="table table-hover mt-lg-5 border border-primary p-3 mx-3">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Pakar</th>
-                                </tr>
-                                <tr>
-                                    <th scope="col">No</th>
-                                    <th scope="col">Identifikasi</th>
-                                    <th scope="col">Nilai (MB - MD)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($pakar as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>
-                                            {{ $item->kode_identifikasi }} | {{ $item->kode_pasal }}
-                                        </td>
-                                        <td>{{ $item->mb - $item->md }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+            <div class="container mt-2 text-center">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card border-primary mb-3 shadow-sm">
+                            <div class="card-header bg-primary text-white">
+                                Pakar
+                            </div>
+                            <div class="card-body">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">No</th>
+                                            <th scope="col">Identifikasi</th>
+                                            <th scope="col">Nilai (MB - MD)</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($pakar as $item)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $item->kode_identifikasi }} | {{ $item->kode_pasal }}</td>
+                                                <td>{{ $item->mb - $item->md }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
 
-                        {{-- User --}}
-                        <table class="table table-hover mt-lg-5 border border-danger p-3 mx-3">
-                            <thead>
-                                <tr>
-                                    <th scope="col">User</th>
-                                </tr>
-                                <tr>
-                                    <th scope="col">Identifikasi</th>
-                                    <th scope="col">Nilai</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($identifikasi_by_user as $key)
-                                    <tr>
-                                        <td>{{ $key[0] }}</td>
-                                        <td>{{ $key[1] }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <div class="col-lg-4">
+                        <div class="card border-danger mb-3 shadow-sm">
+                            <div class="card-header bg-danger text-white">
+                                User
+                            </div>
+                            <div class="card-body">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Identifikasi</th>
+                                            <th scope="col">Nilai</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($identifikasi_by_user as $key)
+                                            <tr>
+                                                <td>{{ $key[0] }}</td>
+                                                <td>{{ $key[1] }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
 
-                        {{-- Tabel Cf Gabungan --}}
-                        {{-- CF Gabungan --}}
-                        <table class="table table-hover mt-lg-5 border border-info p-3 mx-3">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Hasil</th>
-                                </tr>
-                                <tr>
-                                    <th scope="col">Nilai</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($cf_kombinasi['cf'] as $key)
-                                    <tr>
-                                        <td>{{ $key }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <div class="col-lg-4">
+                        <div class="card border-info mb-3 shadow-sm">
+                            <div class="card-header bg-info text-white">
+                                CF Gabungan
+                            </div>
+                            <div class="card-body">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Hasil</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($cf_kombinasi['cf'] as $key)
+                                            <tr>
+                                                <td>{{ $key }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            {{-- section 3 --}}
+            <!-- section 3 -->
             <div class="row">
                 <div class="col-md-10 mx-auto">
-                    <div class="card my-4">
-                        <div class="card-header">
-                            Hasil
+                    <div class="card my-4 shadow-sm">
+                        <div class="card-header text-white bg-primary">
+                            <center>HASIL IDENTIFIKASI</center>
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">
-                                {{ $putusan_dipilih['kode_pasal']->kode_pasal }} |
-                                {{ $putusan_dipilih['kode_pasal']->pasal }}
-                            </h5>
-                            <p class="card-text">Jadi dapat disimpulkan bahwa pelaku tindak pidana kecelakan lalu lintas teridentifikasi pasal tersebut dengan tingkat kepastian pakar hakim sebesar <span
-                                    class="fw-semibold fs-4">{{ round($hasil['value'] * 100, 2) }}</span> %</p>
+                            <table class="table table-hover text-center">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Kode Pasal</th>
+                                        <th scope="col">Pasal</th>
+                                        <th scope="col">Tingkat Kepercayaan (%)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    // Membuat salinan data_putusan untuk diurutkan
+                                    $sortedDataPutusan = $data_putusan;
+                                    
+                                    // Mengurutkan array berdasarkan value secara descending
+                                    usort($sortedDataPutusan, function ($a, $b) {
+                                        return $b['value'] <=> $a['value'];
+                                    });
+                                    ?>
+                                    @foreach ($sortedDataPutusan as $val)
+                                        <?php
+                            // Cek apakah tingkat kepercayaan lebih besar dari -20%
+                            if ($val['value'] * 100 > -20) {
+                            ?>
+                                        <tr>
+                                            <td>{{ $val['kode_pasal'] }}</td>
+                                            <td>{{ App\Models\TingkatPasal::where('kode_pasal', $val['kode_pasal'])->first()->pasal }}
+                                            </td>
+                                            <td>{{ $val['value'] * 100 }} %</td>
+                                        </tr>
+                                        <?php
+                            }
+                            ?>
+                                    @endforeach
+                                </tbody>
+                            </table>
+
+                            <div class="card my-4 shadow">
+                                <div class="card-header text-white bg-dark">
+                                    <center>KESIMPULAN SANKSI HASIL IDENTIFIKASI</center>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        {{ $putusan_dipilih['kode_pasal']->kode_pasal }} |
+                                        {{ $putusan_dipilih['kode_pasal']->pasal }}
+                                    </h5>
+                                    <p style="text-align: justify; class="card-text">Jadi dapat disimpulkan bahwa pelaku
+                                        tindak pidana kecelakan lalu
+                                        lintas
+                                        teridentifikasi pasal tersebut dengan tingkat kepastian pakar hakim sebesar <span
+                                            class="fw-semibold fs-4">{{ round($hasil['value'] * 100, 2) }}</span> %</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/components/cl_article.blade.php
+++ resources/views/components/cl_article.blade.php
@@ -1,10 +1,15 @@
 <div class="row">
     <div class="col-lg-10 mx-auto">
-        <div class="my-4 article border border-warning p-4">
-            <h2 class="mx-auto my-2">{{ $artikel->judul }}</h2>
-            <p class="my-2 py-2">{{ $artikel->isi }}</p>
-            <p class="my-2 py-2">{{ $artikel->referensi }}</p>
-            <p class="text-decoration-underline fst-italic">{{ $artikel->kategori_pelanggaran }}</p>
+        <div class="my-4 card border border-primary shadow-sm">
+            <div class="card-body">
+                <h2 class="card-title text-decoration-underline text-center" style="text-transform: uppercase;">
+                    {{ $artikel->judul }}</h2>
+                <p class="card-text" style="text-align: justify;">{{ $artikel->isi }}</p>
+                <p class="card-text" style="text-align: justify;">{{ $artikel->referensi }}</p>
+                <p class="card-text text-decoration-underline fst-italic" style="text-align: justify;">
+                    {{ $artikel->kategori_pelanggaran }}</p>
+
+            </div>
         </div>
     </div>
 </div>
